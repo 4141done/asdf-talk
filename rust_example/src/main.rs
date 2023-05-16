f8b39aa1696b0aae7caef68c6fc011c89b7184e0
Javier Evans <j.evans@f5.com>
2023-05-16 07:56:38 -0700

    change greeting text

--- rust_example/src/main.rs
+++ rust_example/src/main.rs
@@ -2,7 +2,7 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("hello, {}", name));
+    let hello = warp::path!("hello" / String).map(|name| format!("Howdy, {}", name));
 
     warp::serve(hello).run(([0, 0, 0, 0], 7000)).await;
 }
